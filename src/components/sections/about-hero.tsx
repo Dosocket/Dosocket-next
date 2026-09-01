d0d8dc01463ad85628c9c3e8b8ab507847a80ccf
Ezza Fatima <ezzafatima125@gmail.com>
2026-09-01 18:29:58 +0500

    add imports

--- src/components/sections/about-hero.tsx
+++ src/components/sections/about-hero.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import seniorProductExpert from "../../../public/SeniorProductExpert.png";
 import trustedByStartups from "../../../public/TrustedbyFastGrowingStartups.png";
